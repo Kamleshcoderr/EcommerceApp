3e0e0c4d443158db124490f03fcea3e88c9d8d1b
Kamlesh Kumar <Kamarkamlesh020@gmail.com>
2022-09-13 08:51:07 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -97,9 +97,9 @@ app.post('/photos/upload', upload.array('photos', 12), function (req, res, next)
 
   try{
     let files = req.files;
-    if(!files.length){
-      return res.status(400).json({ err:'Please upload an image', msg:'Please upload an image' })
-    }
+//     if(!files.length){
+//       return res.status(400).json({ err:'Please upload an image', msg:'Please upload an image' })
+//     }
     let file = req.files[0]
     if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
         return res.json({"image" : file.filename}) 
